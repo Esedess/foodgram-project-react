4a36ef7ce863060c72455f2d54c81cbecb2dcbb1
KapkaDibab <xk.8@yandex.ru>
2022-08-24 02:41:52 +0300

    final edits?

--- backend/api/management/commands/import_user.py
+++ backend/api/management/commands/import_user.py
@@ -3,6 +3,7 @@ from csv import DictReader
 from django.conf import settings
 from django.core.management.base import BaseCommand
 from django.db.utils import IntegrityError
+from django.utils.crypto import get_random_string
 from progress.spinner import LineSpinner
 
 from users.models import User
@@ -33,7 +34,7 @@ class Command(BaseCommand):
                 encoding='utf8'
             )
         ):
-            username, email, first_name, last_name, password = row.values()
+            username, email, first_name, last_name = row.values()
             try:
                 user = User(
                     username=username,
@@ -41,7 +42,7 @@ class Command(BaseCommand):
                     first_name=first_name,
                     last_name=last_name,
                 )
-                user.set_password(password)
+                user.set_password(get_random_string(10))
                 user.save()
                 spinner.next()
             except IntegrityError:
